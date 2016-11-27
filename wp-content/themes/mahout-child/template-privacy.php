a58543691835c187ed522a00c3e11c478b61f09d
jason spriggs <jasonspriggs73@gmail.com>
2016-11-27 11:21:46 -0800

    added terms of service

---
+++ wp-content/themes/mahout-child/template-privacy.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Privacy Page */ ?>
+
+<?php get_header(); ?>
+
+
+	<div class="container-fluid ">
+            <div class="row">
+					<div class="col-lg-12">
+						<?php echo do_shortcode( '[my_privacy_policy]' ); ?>
+					</div>
+
+            </div>
+
+	</div>
+
+
+
+
+
+<?php get_footer();?>
---
+++ wp-content/themes/mahout-child/template-terms.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Terms Page */ ?>
+
+<?php get_header(); ?>
+
+
+
+
+
+	<div class="container-fluid ">
+            <div class="row">
+					<div class="col-lg-12">
+						<?php echo do_shortcode( '[my_terms_of_service]' ); ?>
+					</div>
+
+            </div>
+
+	</div>
+
+
+
+<?php get_footer();?>
